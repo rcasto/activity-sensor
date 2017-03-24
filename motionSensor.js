3a594f6bebc67b3c238e41be22154ac56e2aa5eb
Richie Casto <Richie Casto>
2017-03-23 23:38:55 -0700

    handle block time

--- motionSensor.js
+++ motionSensor.js
@@ -2,12 +2,15 @@ var helpers = require('./helpers');
 var events = require('events');
 var rpio = helpers.getRpio(process.platform);
 
+var blockTimeoutTimeInMs = 5000; // 5 seconds
+
 class MotionSensorEmitter extends events.EventEmitter {
     constructor(pin) {
         super();
 
         this.pin = pin;
         this.state = null;
+        this.blockTimeoutId = null;
 
         rpio.open(pin, rpio.INPUT);
 
@@ -17,11 +20,28 @@ class MotionSensorEmitter extends events.EventEmitter {
             console.log('Motion sensor ready');
             this.emit('ready');
             this.readChange();
-            rpio.poll(pin, () => this.readChange());
+            rpio.poll(pin, () => this.handlePinUpdate());
         }, 60 * 1000);
 
         process.on('exit', () => this.cleanup());
         process.on('SIGINT', () => this.cleanup());
+    }
+    handlePinUpdate() {
+        var currentState = this.read();
+        if (this.state !== currentState) {
+            if (currentState === rpio.LOW) {
+                console.log('Starting block timer, but keeping signal active');
+                this.blockTimeoutId = this.blockTimeoutId || setTimeout(() => {
+                    console.log('Block timer over, signal output now low');
+                    this.emit('state', this.state = rpio.LOW);
+                }, blockTimeoutTimeInMs);
+            } else {
+                console.log('Signal output high');
+                clearTimeout(this.blockTimeoutId);
+                this.blockTimeoutId = null;
+                this.emit('state', this.state = rpio.HIGH);
+            }
+        }
     }
     readChange() {
         console.log('Reading motion sensor for change');
